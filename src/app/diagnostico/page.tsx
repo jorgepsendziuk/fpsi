69f67cc9ca627b593e070b3eb77a7c944c0f362a
Jorge Psendziuk <jimxxx@gmail.com>
2025-01-12 17:31:46 -0300

    reactor

--- src/app/diagnostico/page.tsx
+++ src/app/diagnostico/page.tsx
@@ -20,197 +20,8 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { supabaseBrowserClient } from "@utils/supabase/client";
-
-const initialState = {
-  diagnosticos: [],
-  controles: {},
-  medidas: {},
-  respostas: [],
-};
-
-interface Diagnostico {
-  id: number;
-  descricao: string;
-  cor: string;
-  indice: number;
-  maturidade: number;
-}
-
-interface Controle {
-  id: number;
-  diagnostico: number;
-  numero: number;
-  nome: string;
-  nivel: number;
-}
-
-interface Medida {
-  id: number;
-  id_medida: string;
-  id_controle: number;
-  id_cisv8: string;
-  grupo_imple: string;
-  funcao_nist_csf: string;
-  medida: string;
-  descricao: string;
-  resposta: number;
-  justificativa: string;
-}
-
-interface Resposta {
-  id: number;
-  peso: number | null;
-  label: string;
-}
-
-interface State {
-  diagnosticos: Diagnostico[];
-  controles: { [key: number]: Controle[] };
-  medidas: { [key: number]: Medida[] };
-  respostas: Resposta[];
-}
-
-interface Action {
-  type: string;
-  payload?: any;
-  diagnosticoId?: number;
-  controleId?: number;
-  medidaId?: number;
-  field?: string;
-  value?: any;
-}
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case "SET_DIAGNOSTICOS":
-      return { ...state, diagnosticos: action.payload };
-    case "SET_CONTROLES":
-      return {
-        ...state,
-        controles: {
-          ...state.controles,
-          [action.diagnosticoId!]: action.payload,
-        },
-      };
-    case "SET_MEDIDAS":
-      return {
-        ...state,
-        medidas: { ...state.medidas, [action.controleId!]: action.payload },
-      };
-    case "SET_RESPOSTAS":
-      return { ...state, respostas: action.payload };
-    case "UPDATE_MEDIDA":
-      return {
-        ...state,
-        medidas: {
-          ...state.medidas,
-          [action.controleId!]: state.medidas[action.controleId!].map((medida) =>
-            medida.id === action.medidaId
-              ? { ...medida, [action.field!]: action.value }
-              : medida
-          ),
-        },
-      };
-    case "UPDATE_CONTROLE":
-      return {
-        ...state,
-        controles: {
-          ...state.controles,
-          [action.diagnosticoId!]: state.controles[action.diagnosticoId!].map((controle) =>
-            controle.id === action.controleId
-              ? { ...controle, [action.field!]: action.value }
-              : controle
-          ),
-        },
-      };
-    default:
-      return state;
-  }
-}
-
-const maturidade = [
-  {id: 1, min: 0,   max: 0.29, label: "Inicial"},
-  {id: 2, min: 0.3, max: 0.49, label: "Básico"},
-  {id: 3, min: 0.5, max: 0.69, label: "Intermediário"},
-  {id: 4, min: 0.7, max: 0.89, label: "Em Aprimoramento"},
-  {id: 5, min: 0.9, max: 1,    label: "Aprimorado"}
-];
-
-const respostas = [
-  { id: 1, peso: 1,     label: "Adota em maior parte ou totalmente" },
-  { id: 2, peso: 0.75,  label: "Adota em menor parte" },
-  { id: 3, peso: 0.5,   label: "Adota parcialmente" },
-  { id: 4, peso: 0.25,  label: "Há decisão formal ou plano aprovado para implementar"},
-  { id: 5, peso: 0,     label: "A organização não adota essa medida" },
-  { id: 6, peso: null,  label: "Não se aplica" },
-];
-
-const incc = [
-  { id: 1, nivel: 0, indice: 0,   label: "Ausência de capacidade para a implementação das medidas do controle, ou desconhecimento sobre o atendimento das medidas." },
-  { id: 2, nivel: 1, indice: 20,  label: "O controle atinge mais ou menos seu objetivo, por meio da aplicação de um conjunto incompleto de atividades que podem ser caracterizadas como iniciais ou intuitivas (pouco organizadas)." },
-  { id: 3, nivel: 2, indice: 40,  label: "O controle atinge seu objetivo por meio da aplicação de um conjunto básico, porém completo, de atividades que podem ser caracterizadas como realizadas." },
-  { id: 4, nivel: 3, indice: 60,  label: "O controle atinge seu objetivo de forma muito mais organizada utilizando os recursos organizacionais. Além disso, o controle é formalizado por meio de uma política institucional, específica ou como parte de outra maior." },
-  { id: 5, nivel: 4, indice: 80,  label: "O controle atinge seu objetivo, é bem definido e suas medidas são implementadas continuamente por meio de um processo decorrente da política formalizada." },
-  { id: 6, nivel: 5, indice: 100, label: "O controle atinge seu objetivo, é bem definido, suas medidas são implementadas continuamente por meio de um processo e seu desempenho é mensurado quantitativamente por meio de indicadores." },
-];
-
-const respostasimnao = [
-  { id: 1, peso: 1,  label: "Sim" },
-  { id: 2, peso: 0,  label: "Não" },
-];
-
-const calculateSumOfResponses = (medidas: Medida[], diagnostico: number): number => {
-  return medidas.reduce((sum, medida) => {
-    let resposta: Resposta | undefined;
-    if (diagnostico === 1) {
-      resposta = respostasimnao.find((resposta) => resposta.id === medida.resposta);
-    } else if (diagnostico === 2 || diagnostico === 3) {
-      resposta = respostas.find((resposta) => resposta.id === medida.resposta);
-    }
-    return sum + (resposta?.peso || 0);
-  }, 0);
-};
-
-const calculateSumOfResponsesForDiagnostico = (diagnosticoId: number, state: State): string | number => {
-  const controles = state.controles[diagnosticoId] || [];
-  const controleZero: Controle | undefined = controles.find((controle: Controle) => controle.numero === 0);
-  const maturityIndexControleZero = controleZero ? parseFloat(calculateMaturityIndexForControle(controleZero, state)) : 0;
-
-  if (diagnosticoId === 1) {
-    return maturityIndexControleZero;
-  } else if (diagnosticoId === 2 || diagnosticoId === 3) {
-    const maturityIndices: number[] = controles
-      .filter((controle: Controle) => controle.diagnostico === diagnosticoId)
-      .map((controle: Controle) => parseFloat(calculateMaturityIndexForControle(controle, state)));
-
-    const sumOfMaturityIndices = maturityIndices.reduce((sum, index) => sum + index, 0);
-    const numberOfControles = maturityIndices.length;
-
-    return numberOfControles > 0
-      ? (
-          ((maturityIndexControleZero * 4) + sumOfMaturityIndices) / numberOfControles
-        ).toFixed(2)
-      : 0;
-  }
-  return 0;
-};
-
-const calculateMaturityIndexForControle = (controle: Controle, state: State): string => {
-  const medidas = state.medidas[controle.id] || [];
-  const sumOfResponses = calculateSumOfResponses(medidas, controle.diagnostico);
-  const numberOfMedidas = medidas.length;
-  return numberOfMedidas > 0 ? 
-    (
-      ((sumOfResponses / numberOfMedidas) / 2) *
-      (1 + (((incc.find((incc) => incc.id === controle.nivel)?.nivel || 0)) * 1 / 5))
-    ).toFixed(2) 
-    : "0";
-};
-
-const getMaturityLabel = (indice: number): string => {
-  const maturity = maturidade.find(m => indice >= m.min && indice <= m.max);
-  return maturity ? maturity.label : "";
-};
+import { initialState, reducer, State, Action } from "./state";
+import { calculateSumOfResponsesForDiagnostico, getMaturityLabel, incc, respostas, respostasimnao, calculateMaturityIndexForControle } from "./utils";
 
 const DiagnosticoPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -251,7 +62,7 @@ const DiagnosticoPage = () => {
     };
 
     fetchDiagnosticos();
-    //fetchRespostas();
+    fetchRespostas();
     fetchControlesAndMedidas();
   }, []);
 
@@ -287,19 +98,19 @@ const DiagnosticoPage = () => {
     });
   };
 
-  // const handleJustificativaChange = async (medidaId: number, controleId: number, newValue: string): Promise<void> => {
-  //   await supabaseBrowserClient
-  //     .from("medida")
-  //     .update({ justificativa: newValue })
-  //     .eq("id", medidaId);
-  //   dispatch({
-  //     type: "UPDATE_MEDIDA",
-  //     medidaId,
-  //     controleId,
-  //     field: "justificativa",
-  //     value: newValue,
-  //   });
-  // };
+  const handleJustificativaChange = async (medidaId: number, controleId: number, newValue: string): Promise<void> => {
+    await supabaseBrowserClient
+      .from("medida")
+      .update({ justificativa: newValue })
+      .eq("id", medidaId);
+    dispatch({
+      type: "UPDATE_MEDIDA",
+      medidaId,
+      controleId,
+      field: "justificativa",
+      value: newValue,
+    });
+  };
 
   const handleINCCChange = async (controleId: number, diagnosticoId: number, newValue: number): Promise<void> => {
     await supabaseBrowserClient
@@ -317,17 +128,10 @@ const DiagnosticoPage = () => {
 
   return (
     <div>
-
       {state.diagnosticos.map((diagnostico: any) => (
         <Accordion
           slotProps={{ transition: { unmountOnExit: true } }}
-          //component={Paper}
-          //elevation={10}
-          style={{
-            backgroundColor: diagnostico.cor,
-            //margin: "0px",
-            
-          }}
+          style={{ backgroundColor: diagnostico.cor }}
           key={diagnostico.id}
           onChange={() => handleControleFetch(diagnostico.id)}
         >
@@ -336,7 +140,6 @@ const DiagnosticoPage = () => {
               <Typography
                 variant="h6"
                 style={{ width: "80%", fontWeight: "800", padding: "10px" }}
-                
               >
                 {diagnostico.descricao}
               </Typography>
@@ -349,10 +152,7 @@ const DiagnosticoPage = () => {
                 bgcolor: "#ffffff",
               }}
             >
-              <Typography
-                align="center"
-                style={{ fontWeight: "400",  }}
-              >
+              <Typography align="center" style={{ fontWeight: "400" }}>
                 MATURIDADE
               </Typography>
               <Typography
@@ -399,7 +199,7 @@ const DiagnosticoPage = () => {
             <TableContainer
               component={Paper}
               elevation={10}
-             style={{ backgroundColor: "#dadada", margin: "0px" }}
+              style={{ backgroundColor: "#dadada", margin: "0px" }}
             >
               <Table>
                 <TableBody>
@@ -416,7 +216,7 @@ const DiagnosticoPage = () => {
                           {controle.numero}
                         </Typography>
                       </TableCell>
-                      <TableCell >
+                      <TableCell>
                         <Accordion
                           slotProps={{ transition: { unmountOnExit: true } }}
                           key={controle.id}
@@ -450,21 +250,21 @@ const DiagnosticoPage = () => {
                                 </Box>
                                 <Box sx={{}}>
                                   <Typography variant="caption">
-                                  <Select
-                                    value={controle.nivel || ""}
-                                    onClick={(event) => event.stopPropagation()}
-                                    onChange={(event) =>
-                                      handleINCCChange(controle.id, diagnostico.id, parseInt(event.target.value.toString(), 10))
-                                    }
-                                  >
-                                    {incc.map((incc) => (
-                                      <MenuItem key={incc.id} value={incc.id}>
-                                        <Typography sx={{ whiteSpace: "normal" }}>
-                                          <b>{incc.nivel}</b> - {incc.label}
-                                        </Typography>
-                                      </MenuItem>
-                                    ))}
-                                  </Select>
+                                    <Select
+                                      value={controle.nivel || ""}
+                                      onClick={(event) => event.stopPropagation()}
+                                      onChange={(event) =>
+                                        handleINCCChange(controle.id, diagnostico.id, parseInt(event.target.value.toString(), 10))
+                                      }
+                                    >
+                                      {incc.map((incc) => (
+                                        <MenuItem key={incc.id} value={incc.id}>
+                                          <Typography sx={{ whiteSpace: "normal" }}>
+                                            <b>{incc.nivel}</b> - {incc.label}
+                                          </Typography>
+                                        </MenuItem>
+                                      ))}
+                                    </Select>
                                   </Typography>
                                 </Box>
                               </Box>
@@ -535,7 +335,6 @@ const DiagnosticoPage = () => {
                                         parseInt(event.target.value.toString(), 10)
                                       )
                                     }
-                                    
                                   >
                                     {(controle.diagnostico === 1 ? respostasimnao : respostas).map((respostas) => (
                                       <MenuItem key={respostas.id} value={respostas.id}>
@@ -591,14 +390,14 @@ const DiagnosticoPage = () => {
                                     color="primary"
                                     value={medida.justificativa || ""}
                                     multiline
-                                    //focused
-                                    // onChange={(event) =>
-                                    //   handleJustificativaChange(
-                                    //     medida.id,
-                                    //     controle.id,
-                                    //     event.target.value
-                                    //   )
-                                    // }
+                                    focused
+                                    onChange={(event) =>
+                                      handleJustificativaChange(
+                                        medida.id,
+                                        controle.id,
+                                        event.target.value
+                                      )
+                                    }
                                   />
                                   <TextField
                                     style={{ width: "40%", padding: 10 }}
